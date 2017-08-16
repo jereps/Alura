3cd1aa54e8be7a66305499cbb7f6be70f26886be
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2017-08-16 16:36:24 -0300

    Alteração do Arquivo Principal.java

--- Conta/src/Principal.java
+++ Conta/src/Principal.java
@@ -2,7 +2,11 @@ public class Principal {
 	public static void main(String[] args) {
 		Conta conta = new Conta();
 		conta.deposita(100.0);
+		
+		Conta conta2 = new Conta();
+		conta2.deposita(100.5);
 
 		System.out.println(conta.getSaldo());
+		System.out.println(conta2.getSaldo());
 	}
 }
