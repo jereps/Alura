f63db529522b5d617987e9e8d2ae73dd7031db21
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2017-08-18 15:40:05 -0300

    exercicio 6 JavaII Aula6

--- Conta/src/Conta.java
+++ Conta/src/Conta.java
@@ -1,20 +1,24 @@
 
 public abstract class Conta {
-	
+
 	protected double saldo;
 
 	void deposita(double valor) {
-		this.saldo = valor;
+		if (valor < 0) {
+			throw new IllegalArgumentException();
+		} else {
+			this.saldo += valor;
+		}
 	}
 
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public void saca(double valor) {
-		this.saldo += valor;
+		this.saldo -= valor;
 	}
-	
+
 	public abstract void atualiza(double taxa);
-	
+
 }
