73b1e3fea3343c6666ba22e8df1f4df8414fdb05
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2017-08-18 15:48:52 -0300

    exercĩcio 8 JavaII Aula6

--- Conta/src/TestaDeposita.java
+++ Conta/src/TestaDeposita.java
@@ -2,6 +2,10 @@
 public class TestaDeposita {
 	public static void main(String[] args) {
 		Conta cp = new ContaCorrente();
+		try {
 		cp.deposita(-100);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Valor negativo não é permitido: " + e);
+		}
 	}
 }
