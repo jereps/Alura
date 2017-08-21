ff58e8ec4390116105a157e3a6b4a91d4c6121e2
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2017-08-21 19:32:07 -0300

    Adicionado javadoc do classe Conta

--- Conta/src/br/com/empresa/conta/Conta.java
+++ Conta/src/br/com/empresa/conta/Conta.java
@@ -1,9 +1,21 @@
 package br.com.empresa.conta;
 
+/**
+ * Classe responsavel por moldar as Contas do Banco
+ * 
+ * @author Jeremias Ponciano da Silva
+ *
+ */
+
 public abstract class Conta {
 
 	protected double saldo;
 
+	
+	/**
+	 * Metodo que incrementa o saldo.
+	 * @param valor.
+	 */
 	public void deposita(double valor) {
 		if (valor < 0) {
 			throw new ValorInvalidoException(valor);
