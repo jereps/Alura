2ee0e444e8dd852e69a41f24f3998c0241580238
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2017-08-31 18:06:20 -0300

    alteração no programa

--- Python/raspagem.py
+++ Python/raspagem.py
@@ -1,25 +1,34 @@
 import requests
 from lxml import html
 import lxml.html as parser
+import re
+from selenium import webdriver
 
 
- 
-def cria_lista_links(content):
+def cria_lista_links(r):
+    '''site = r.content'''
+    driver = webdriver.Chrome()
+    driver.get(r)
     links = []
-    html = parser.fromstring(content)
+    html = parser.fromstring(driver.page_source)
     linkss = html.xpath('//a/@href')
+
     for link in linkss:
        links.append("link")
 
-    arq = open("fatec.txt", "w")
+    for linknovo in link:
+        cria_lista_links(r + linknovo)
+    '''nomearq = "fatec0.txt"
+
+    arq = open(nomearq, "w")
 
     for lin in linkss:
-       arq.write(lin)
-       arq.write('\n')
-    
-    arq.close()
-    
+       if ('http://' not in lin and 'https://' not in lin):
+          arq.write(r + lin)
+          arq.write('\n')
+
+    arq.close()'''
     return links
 
-r = requests.get("http://fatecsjc-prd.azurewebsites.net/")
-print '\n'.join(cria_lista_links(r.content))
+r = "http://fatecsjc-prd.azurewebsites.net/"
+print '\n'.join(cria_lista_links(r))
