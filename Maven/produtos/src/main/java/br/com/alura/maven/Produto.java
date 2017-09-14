95627946391507d6a60ea6039085ccc3c7f2d964
Jeremias Ponciano Da Silva <jeremiason2911@gmail.com>
2017-09-14 17:23:36 -0300

    Alteração no Projeto produtos

--- Maven/produtos/src/main/java/br/com/alura/maven/Produto.java
+++ Maven/produtos/src/main/java/br/com/alura/maven/Produto.java
@@ -10,6 +10,19 @@ public class Produto {
 		this.nome = nome;
 		this.preco = preco;
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public double getPrecoComImposto() {
+		return preco * 1.1;
+	}
+	
 	
 	
 }
